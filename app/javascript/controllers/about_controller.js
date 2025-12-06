8489ad0d4baeb2762a79d1f0823741b180463243
William Sebastian <okeefesebastian91@gmail.com>
2025-12-06 20:45:27 +0900

    fixed the about toggle

--- app/javascript/controllers/about_controller.js
+++ app/javascript/controllers/about_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["content", "button"]
 
-  toggle(event) {
+  connect() {
+    console.log("About controller connected")
+  }
+
+  show(event) {
     event.preventDefault()
-    this.contentTarget.classList.toggle("hidden")
+    this.contentTarget.classList.remove("hidden")
+    this.buttonTarget.classList.add("active")
+  }
+
+  close() {
+    this.contentTarget.classList.add("hidden")
+    this.buttonTarget.classList.remove("active")
   }
 }
