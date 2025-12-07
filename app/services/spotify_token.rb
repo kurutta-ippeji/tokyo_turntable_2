5ad7a4d65ee8ceaab93709e3fa022d21073a485d
William Sebastian <okeefesebastian91@gmail.com>
2025-12-07 21:17:17 +0900

    created spotify token fetcher

---
+++ app/services/spotify_token.rb
@@ -0,0 +1,18 @@
+class SpotifyToken
+  TOKEN_URL = URI("https://accounts.spotify.com/api/token")
+
+  def self.fetch
+    client_id = Rails.application.credentials.spotify[:client_id]
+    client_secret = Rails.application.credentials.spotify[:client_secret]
+
+    req = Net::HTTP::Post.new(TOKEN_URL)
+    req.basic_auth(client_id, client_secret)
+    req.set_form_data({ "grant_type" => "client_credentials" })
+
+    res = Net::HTTP.start(TOKEN_URL.hostname, TOKEN_URL.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+
+    JSON.parse(res.body)["access_token"]
+  end
+end
